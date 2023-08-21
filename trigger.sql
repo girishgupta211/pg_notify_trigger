c12056b0fd574367ab856b87396cd1f663982fba
Girish Gupta <67008135+girishgupta211@users.noreply.github.com>
2023-08-21 15:12:24 +0530

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,3 +1,33 @@
+-- The code you provided is for creating a PostgreSQL trigger and a trigger function to notify clients of updates to the `colleges` table using the `pg_notify` function. This is often used for implementing real-time updates or change notifications in a database-driven application.
+
+-- **1. Trigger Function (`colleges_notify_trigger`):**
+
+-- This is a PostgreSQL trigger function written in PL/pgSQL, which is a procedural language used for writing database functions and triggers.
+
+-- - The `CREATE OR REPLACE FUNCTION` statement creates or replaces the function named `colleges_notify_trigger()`.
+-- - Inside the function, the `BEGIN` and `END` blocks enclose the code logic.
+-- - The `PERFORM pg_notify(...)` line triggers a PostgreSQL notification using the `pg_notify` function. It sends the `NEW` row (the updated row) as a JSON-formatted string to the specified channel, which is `'update_notification'` in this case.
+-- - The `RETURN new;` line indicates that the original operation (in this case, an update) should proceed normally.
+
+-- **2. Trigger Creation (`colleges_update_trigger`):**
+
+-- This creates a trigger that fires the `colleges_notify_trigger` function when specific conditions are met:
+
+-- - The trigger is named `colleges_update_trigger`.
+-- - It is defined to execute `AFTER UPDATE` on the `colleges` table.
+-- - The `FOR EACH row` clause specifies that the trigger is fired for each row that is updated.
+-- - The condition `WHEN (OLD.name IS DISTINCT FROM NEW.name)` indicates that the trigger should fire only when the `name` column is updated and its old and new values are different.
+-- - The `EXECUTE PROCEDURE public.colleges_notify_trigger();` line specifies that the `colleges_notify_trigger` function should be executed when the trigger conditions are met.
+
+-- **3. Trigger Removal (`DROP TRIGGER`):
+
+-- This is used to remove the previously created trigger:
+
+-- - The `DROP TRIGGER` statement removes the `colleges_update_trigger` trigger from the `public.colleges` table.
+
+-- In summary, the provided code creates a trigger function that sends notifications when updates occur on the `colleges` table, specifically when the `name` column changes. This can be useful for building real-time applications that need to respond to changes in the database. The trigger function uses the `pg_notify` function to send notifications, and the trigger is activated when specific conditions are met during an `UPDATE` operation.
+
+  
 CREATE or REPLACE FUNCTION public.colleges_notify_trigger() RETURNS trigger AS $$
 DECLARE
 BEGIN
@@ -36,3 +66,6 @@ QUERY PLAN | Execution time: 0.127 ms
 -- created_at     | 2021-03-10 20:52:38.116151
 -- updated_at     | 2021-03-10 20:52:38.116151
 -- workflow_state | active
+
+
+
