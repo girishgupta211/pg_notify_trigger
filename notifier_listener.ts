9f16de747f2f3f93ec811842f03b9f05cdcb9705
Girish Gupta <67008135+girishgupta211@users.noreply.github.com>
2023-08-21 15:09:32 +0530

    Update notifier_listener.ts

--- notifier_listener.ts
+++ notifier_listener.ts
@@ -1,1 +1,26 @@
+import { Client } from 'pg';
 
+const client = new Client({
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: 'postgres',
+      database: 'postgres'
+  });
+
+    // const client = LtiDb.client
+    client.connect((err: any) => {
+      if (err) {
+          console.log('Error in connecting database: ', err);
+      } else {
+          console.log('Database connected');
+  
+          client.on('notification', (msg: any) => {
+              console.log('msg.payload');
+              console.log(msg.payload);
+          });
+  
+          const query = client.query('LISTEN update_notification');
+      }
+  });
+  
